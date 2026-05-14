e5849bc9912af67a6d68da6f8355d77220c5b7c9
navyfish216 <navyfish@kbd.biglobe.ne.jp>
2026-05-14 19:37:49 +0900

    React修行②：stateを使用して状態を管理

--- study-react-app/src/App.jsx
+++ study-react-app/src/App.jsx
@@ -1,20 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import UserList from "./pages/UserList"
-import './App.css'
+//import { useState } from 'react';
+//import reactLogo from './assets/react.svg';
+//import viteLogo from './assets/vite.svg';
+//import heroImg from './assets/hero.png';
+import UserList from "./pages/UserList";
+import CounterApp from "./components/CounterApp";
+import TextInputSample from "./components/TextInputSample";
+import ToggleSwitch from "./components/ToggleSwitch";
+import './App.css';
 
 function App() {
-  // JavaScriptのイベント処理関数（クリックされたときにアラートを出す）
-  const handleClick = () => {
-    alert("クリックされました！");
-  };
-
   return (
     <>
-      <UserList />
+      <ToggleSwitch />
+      <TextInputSample />
+      <CounterApp />
+      <UserList /> 
     </>
-  )
+  );
 }
 export default App
---
+++ study-react-app/src/components/TextInputSample.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+
+function TextInputSample() {
+  // ①ステートの初期値は空文字
+  const [text1, setText] = useState('');
+
+  const handleChange = (event) => {
+    // ②入力内容でステートを更新
+    setText(event.target.value);
+  };
+
+  return (
+    <div>
+      <h2>テキスト入力フォーム</h2>
+      {/* ③入力値　④イベント */}
+      <input type="text" value={text1} onChange={handleChange} />
+      {/* ⑤表示 */}
+      <p>あなたが入力した文字: {text1}</p>
+    </div>
+  );
+}
+export default TextInputSample
